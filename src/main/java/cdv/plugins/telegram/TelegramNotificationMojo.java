b197f92a48d0179016055117b3e0e695b7593fc1
coolga <coolgadv@gmail.com>
2016-11-19 17:57:53 +0300

    Message sending

--- src/main/java/cdv/plugins/telegram/TelegramNotificationMojo.java
+++ src/main/java/cdv/plugins/telegram/TelegramNotificationMojo.java
@@ -1,9 +1,11 @@
 package cdv.plugins.telegram;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Mojo for sending Telegram messages
@@ -14,8 +16,24 @@ import org.apache.maven.plugins.annotations.Mojo;
 @Mojo(name = "send-message", threadSafe = true, requiresOnline = true)
 public class TelegramNotificationMojo extends AbstractMojo {
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Hello from telegram plugin");
+    @Parameter(required = true)
+    private String botToken;
+
+    @Parameter(required = true)
+    private String chatId;
+
+    @Parameter(required = true)
+    private String text;
+
+    public void execute() {
+        ClientBuilder.newClient()
+                .target("https://api.telegram.org")
+                .path("bot" + botToken)
+                .path("sendMessage")
+                .queryParam("chat_id", chatId)
+                .queryParam("text", text)
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get();
     }
 
 }
