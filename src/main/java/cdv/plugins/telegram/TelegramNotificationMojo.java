20bfc6abbc97f16c955a2f5f739474272ddc2469
coolga <coolgadv@gmail.com>
2016-11-20 08:14:50 +0300

    Parameters description and logging

--- src/main/java/cdv/plugins/telegram/TelegramNotificationMojo.java
+++ src/main/java/cdv/plugins/telegram/TelegramNotificationMojo.java
@@ -1,6 +1,8 @@
 package cdv.plugins.telegram;
 
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
@@ -16,24 +18,42 @@ import javax.ws.rs.core.MediaType;
 @Mojo(name = "send-message", threadSafe = true, requiresOnline = true, aggregator = true)
 public class TelegramNotificationMojo extends AbstractMojo {
 
+    /**
+     * Telegram Bot API token
+     */
     @Parameter(property = "bot.token", required = true)
     private String botToken;
 
+    /**
+     * Chat identifier
+     */
     @Parameter(property = "chat.id", required = true)
     private String chatId;
 
+    /**
+     * Message text
+     */
     @Parameter(property = "text", required = true)
     private String text;
 
-    public void execute() {
-        ClientBuilder.newClient()
-                .target("https://api.telegram.org")
-                .path("bot" + botToken)
-                .path("sendMessage")
-                .queryParam("chat_id", chatId)
-                .queryParam("text", text)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .get();
+    public void execute() throws MojoExecutionException {
+        Log log = getLog();
+        log.info("Sending notification to Telegram...");
+        log.debug("Message text: " + text);
+        try {
+            String response = ClientBuilder.newClient()
+                    .target("https://api.telegram.org")
+                    .path("bot" + botToken)
+                    .path("sendMessage")
+                    .queryParam("chat_id", chatId)
+                    .queryParam("text", text)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(String.class);
+            log.info("Notification successfully sent");
+            log.debug("Response from Telegram: " + response);
+        } catch (Exception ex) {
+            throw new MojoExecutionException("Notification failure", ex);
+        }
     }
 
 }
